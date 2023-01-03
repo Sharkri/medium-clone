e66a9112c7729dacc96c3b2d4cfdeb3a99f16c9d
Sharkri <sharkdude955@gmail.com>
2023-01-03 05:21:51 -0600

    Organize files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
-import Home from "./Home";
+import "./css/App.css";
+import Home from "./components/Home";
 
 function App() {
   return (
