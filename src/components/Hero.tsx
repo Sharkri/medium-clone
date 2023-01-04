7a00a33b31c27ba8286184880f217ce7d3c36ad6
Sharkri <sharkdude955@gmail.com>
2023-01-04 10:00:52 -0600

    Align hero text to center

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,12 +3,13 @@ import "../css/Hero.css";
 function Hero() {
   return (
     <div className="hero">
-      <h2 className="hero-text">Stay curious.</h2>
-      <h3 className="hero-subtext">
-        Discover stories, thinking, and expertise from writers on any topic.
-      </h3>
-
-      <button className="start-reading">Start reading</button>
+      <div className="hero-content">
+        <h2 className="hero-text">Stay curious.</h2>
+        <h3 className="hero-subtext">
+          Discover stories, thinking, and expertise from writers on any topic.
+        </h3>
+        <button className="start-reading">Start reading</button>
+      </div>
     </div>
   );
 }
