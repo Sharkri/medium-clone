ea1ac98a4ab46766b2e14f83454c3fc76a24ed69
Sharkri <sharkdude955@gmail.com>
2023-01-03 22:40:55 -0600

    Add new font and apply it to hero subtext

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,8 +2,11 @@ import "../css/Hero.css";
 
 function Hero() {
   return (
-    <div>
+    <div className="hero">
       <h2 className="hero-text">Stay curious.</h2>
+      <h3 className="hero-subtext">
+        Discover stories, thinking, and expertise from writers on any topic.
+      </h3>
     </div>
   );
 }
