79d5ca3041b24ae2ae89c07cf4ba8a2d90583d14
Sharkri <sharkdude955@gmail.com>
2023-01-06 04:51:39 -0600

    Install fontawesome package

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./css/App.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import Home from "./components/Home";
 
 function App() {
