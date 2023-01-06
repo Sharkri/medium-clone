339388fae04bb65cbe895f57866b8688b4ce0bce
Sharkri <sharkdude955@gmail.com>
2023-01-06 09:54:41 -0600

    Re-add AuthenticationModal.tsx

---
+++ src/components/AuthenticationModal.tsx
@@ -0,0 +1,5 @@
+function AuthenticationModal() {
+  return <div className="authentication-modal modal"></div>;
+}
+
+export default AuthenticationModal;
--- src/components/SignUpModal.tsx
+++ src/components/SignUpModal.tsx
@@ -1,18 +1,16 @@
 export default function SignUpModal() {
   return (
-    <div className="modal sign-up-modal">
-      <ul>
-        <li>
-          <button>
-            <i className="fa-brands fa-google"></i> sign up with gogle
-          </button>
-        </li>
-        <li>
-          <button>
-            <i className="fa-regular fa-envelope"></i>sign up with email
-          </button>
-        </li>
-      </ul>
-    </div>
+    <ul>
+      <li>
+        <button>
+          <i className="fa-brands fa-google"></i> sign up with gogle
+        </button>
+      </li>
+      <li>
+        <button>
+          <i className="fa-regular fa-envelope"></i>sign up with email
+        </button>
+      </li>
+    </ul>
   );
 }
