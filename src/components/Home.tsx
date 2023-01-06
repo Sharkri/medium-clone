f66857eab236f77703043c40eb3e8553523b88a8
Sharkri <sharkdude955@gmail.com>
2023-01-06 15:00:21 -0600

    Integrate ModalContext into Home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,16 +1,20 @@
 import Header from "./Header";
 import Hero from "./Hero";
 import HomeFeed from "./HomeFeed";
+import Modal from "./modal/Modal";
+import { ModalProvider } from "./modal/ModalContext";
 
 function Home() {
   return (
-    <>
+    <ModalProvider>
+      <Modal />
+
       <Header />
       <main>
         <Hero />
         <HomeFeed />
       </main>
-    </>
+    </ModalProvider>
   );
 }
 
