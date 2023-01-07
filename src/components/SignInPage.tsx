a199d17a8639c5e5d2359e4991bcfb250491c030
Sharkri <sharkdude955@gmail.com>
2023-01-07 12:00:00 -0600

    Add fake tos and Privacy Policy text

--- src/components/SignInPage.tsx
+++ src/components/SignInPage.tsx
@@ -6,24 +6,33 @@ export default function SignInPage() {
   return (
     <div className="sign-in-page">
       <h2 className="serif sign-in-heading">Welcome back.</h2>
-      <ul>
-        <li>
-          <button>
-            <i className="fa-brands fa-google"></i> sign in with gogle
-          </button>
-        </li>
-        <li>
-          <button>
-            <i className="fa-regular fa-envelope"></i>sign in with email
-          </button>
-        </li>
-      </ul>
 
-      <p>
-        No account?{" "}
-        <OpenModalButton element={<SignUpPage />}>
-          <b>Create one</b>
-        </OpenModalButton>
+      <div>
+        <ul>
+          <li>
+            <button>
+              <i className="fa-brands fa-google"></i> sign in with gogle
+            </button>
+          </li>
+          <li>
+            <button>
+              <i className="fa-regular fa-envelope"></i>sign in with email
+            </button>
+          </li>
+        </ul>
+        <p>
+          No account?{" "}
+          <OpenModalButton element={<SignUpPage />}>
+            <b>Create one</b>
+          </OpenModalButton>
+        </p>
+      </div>
+
+      <p className="small-text">
+        Click “Sign In” to agree to Medium’s{" "}
+        <span className="underline">Terms of Service</span> and acknowledge that
+        Medium’s <span className="underline">Privacy Policy</span> applies to
+        you.
       </p>
     </div>
   );
--- src/components/SignUpPage.tsx
+++ src/components/SignUpPage.tsx
@@ -6,23 +6,31 @@ export default function SignUpPage() {
   return (
     <div className="sign-up-page">
       <h2 className="serif sign-up-heading">Join Medium.</h2>
-      <ul>
-        <li>
-          <button>
-            <i className="fa-brands fa-google"></i> sign up with gogle
-          </button>
-        </li>
-        <li>
-          <button>
-            <i className="fa-regular fa-envelope"></i>sign up with email
-          </button>
-        </li>
-      </ul>
-      <p>
-        Already have an account?{" "}
-        <OpenModalButton element={<SignInPage />}>
-          <b>Sign In</b>
-        </OpenModalButton>
+      <div>
+        <ul>
+          <li>
+            <button>
+              <i className="fa-brands fa-google"></i> sign up with gogle
+            </button>
+          </li>
+          <li>
+            <button>
+              <i className="fa-regular fa-envelope"></i>sign up with email
+            </button>
+          </li>
+        </ul>
+        <p>
+          Already have an account?{" "}
+          <OpenModalButton element={<SignInPage />}>
+            <b>Sign In</b>
+          </OpenModalButton>
+        </p>
+      </div>
+      <p className="small-text">
+        Click “Sign Up" to agree to Medium’s{" "}
+        <span className="underline">Terms of Service</span> and acknowledge that
+        Medium’s <span className="underline">Privacy Policy</span> applies to
+        you.
       </p>
     </div>
   );
