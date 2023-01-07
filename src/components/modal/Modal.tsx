83942af2da35aa33d1a9dfdace4606fb24561b04
Sharkri <sharkdude955@gmail.com>
2023-01-07 08:37:17 -0600

    Add close modal button

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -3,10 +3,16 @@ import ModalContext from "./ModalContext";
 import "../../css/Modal.css";
 
 function Modal() {
-  const { isModalOpen, modalContent } = useContext(ModalContext);
+  const { isModalOpen, modalContent, setModalOpen } = useContext(ModalContext);
   return isModalOpen ? (
     <div className="modal">
-      <div className="modal-content">{modalContent}</div>
+      <div className="modal-content">
+        <button className="close-modal" onClick={() => setModalOpen(false)}>
+          <i className="fa-solid fa-xmark" />
+        </button>
+
+        {modalContent}
+      </div>
     </div>
   ) : null;
 }
