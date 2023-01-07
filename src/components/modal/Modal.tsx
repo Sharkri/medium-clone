8438a0bd2b5302a29d344657539181244e08a61c
Sharkri <sharkdude955@gmail.com>
2023-01-07 08:52:27 -0600

    Close modal when click outside

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -5,7 +5,15 @@ import "../../css/Modal.css";
 function Modal() {
   const { isModalOpen, modalContent, setModalOpen } = useContext(ModalContext);
   return isModalOpen ? (
-    <div className="modal">
+    <div
+      className="modal"
+      data-testid="modal"
+      onClick={(e) => {
+        const { className } = e.target as HTMLInputElement;
+        // Close modal if clicked outside of content
+        if (className === "modal") setModalOpen(false);
+      }}
+    >
       <div className="modal-content">
         <button
           className="close-modal"
--- src/components/tests/Modal.test.tsx
+++ src/components/tests/Modal.test.tsx
@@ -6,27 +6,50 @@ import "@testing-library/jest-dom";
 
 const setModalOpen = jest.fn();
 
-it("should close modal when close modal button is clicked", () => {
-  render(
-    <ModalContext.Provider
-      value={{
-        setModalOpen,
-        isModalOpen: true,
-        modalContent: <div>content!</div>,
-      }}
-    >
-      <Modal />
-    </ModalContext.Provider>
-  );
+describe("Close Modal", () => {
+  it("should close modal when close modal button is clicked", () => {
+    render(
+      <ModalContext.Provider
+        value={{
+          setModalOpen,
+          isModalOpen: true,
+          modalContent: <div>content!</div>,
+        }}
+      >
+        <Modal />
+      </ModalContext.Provider>
+    );
 
-  expect(setModalOpen).not.toBeCalled();
+    expect(setModalOpen).not.toBeCalled();
 
-  const closeModalButton = screen.getByRole("button", { name: "close" });
+    const closeModalButton = screen.getByRole("button", { name: "close" });
 
-  userEvent.click(closeModalButton);
+    userEvent.click(closeModalButton);
 
-  // should set modal open to false
-  expect(setModalOpen).toBeCalledWith(false);
+    // should set modal open to false
+    expect(setModalOpen).toBeCalledWith(false);
+  });
+
+  it("closes modal when outside of modal is clicked", () => {
+    render(
+      <ModalContext.Provider
+        value={{
+          setModalOpen,
+          isModalOpen: true,
+          modalContent: <div>content!</div>,
+        }}
+      >
+        <Modal />
+      </ModalContext.Provider>
+    );
+
+    expect(setModalOpen).not.toBeCalled();
+
+    // click on modal instead of modal content
+    userEvent.click(screen.getByTestId("modal"));
+
+    expect(setModalOpen).toBeCalledWith(false);
+  });
 });
 
 it("does not show modalContent when isModalOpen is false", () => {
