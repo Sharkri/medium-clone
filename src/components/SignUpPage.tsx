fd4bae94ec5c984b8540311f2a70b0388262a25f
Sharkri <sharkdude955@gmail.com>
2023-01-07 07:57:05 -0600

    Add "Already have an account?" button

--- src/components/SignUpPage.tsx
+++ src/components/SignUpPage.tsx
@@ -1,16 +1,25 @@
+import OpenModalButton from "./OpenModalButton";
+import SignInPage from "./SignInPage";
+
 export default function SignUpPage() {
   return (
-    <ul>
-      <li>
-        <button>
-          <i className="fa-brands fa-google"></i> sign up with gogle
-        </button>
-      </li>
-      <li>
-        <button>
-          <i className="fa-regular fa-envelope"></i>sign up with email
-        </button>
-      </li>
-    </ul>
+    <div className="sign-up-page">
+      <ul>
+        <li>
+          <button>
+            <i className="fa-brands fa-google"></i> sign up with gogle
+          </button>
+        </li>
+        <li>
+          <button>
+            <i className="fa-regular fa-envelope"></i>sign up with email
+          </button>
+        </li>
+      </ul>
+      <p>
+        Already have an account?{" "}
+        <OpenModalButton element={<SignInPage />}>Sign In</OpenModalButton>
+      </p>
+    </div>
   );
 }
