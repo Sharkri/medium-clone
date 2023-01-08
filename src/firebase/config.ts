bbbc43fc768ebe2a1a88460993f9850bcb51fc4a
Sharkri <sharkdude955@gmail.com>
2023-01-08 15:00:10 -0600

    Add firebase config

---
+++ src/firebase/config.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAD2_5njZVMcPFmlymF-t590iuUbmKaSSc",
+  authDomain: "medium-clone-469b9.firebaseapp.com",
+  projectId: "medium-clone-469b9",
+  storageBucket: "medium-clone-469b9.appspot.com",
+  messagingSenderId: "176786971428",
+  appId: "1:176786971428:web:6919ab946aa228ef0bb1b1",
+  measurementId: "G-4PCF1WCZQ6",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
