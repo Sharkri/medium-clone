26919b33a6a4233631618645a00a2b74e1f3d667
Sharkri <sharkdude955@gmail.com>
2023-02-19 05:11:49 -0600

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      animation: {
+        "spin-horizontal": "spin 1s linear infinite",
+      },
+      keyframes: {
+        spin: {
+          "0%": { transform: "rotateY(0deg)" },
+          "100%": { transform: "rotateY(360deg)" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
