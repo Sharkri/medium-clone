7f25239e989b597b5c64409ac901c852a3c2fbd0
Sharkri <sharkdude955@gmail.com>
2023-02-19 21:06:03 -0600

    Fix incorrect values

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,13 +9,13 @@ function Hero() {
         <h2 className=" text-8xl tracking-tighter mb-4 font-serif">
           Stay curious.
         </h2>
-        <h3 className="text-2xl max-w-[420px] mb-14">
+        <h3 className="text-2xl max-w-[420px] mb-[50px] text-lighterblack">
           Discover stories, thinking, and expertise from writers on any topic.
         </h3>
 
         <OpenModalButton
           element={<SignUpPage />}
-          className="bg-lightblack border-[1px] border-lightblack text-white rounded-full transition duration-[0.25s] w-[213px] leading-6 pt-[7px] px-[20px] pb-[9px] hover:bg-black hover:border-black"
+          className="text-[20px] bg-lightblack border-[1px] border-lightblack text-white rounded-full transition duration-[0.25s] w-[213px] leading-6 pt-[7px] px-[20px] pb-[9px] hover:bg-black hover:border-black"
         >
           Start reading
         </OpenModalButton>
