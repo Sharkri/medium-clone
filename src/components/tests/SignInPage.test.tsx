0dbe997dd62ddaefa676c3d68ac887b2796d6c5f
Sharkri <sharkdude955@gmail.com>
2023-02-23 20:18:53 -0600

    Change color to green on hover

--- src/components/tests/SignInPage.test.tsx
+++ src/components/tests/SignInPage.test.tsx
@@ -13,7 +13,7 @@ it('navigates to <SignUpPage /> when "No account? Create one" is clicked', () =>
   expect(openModalButton.mock.calls[0][0]).toMatchInlineSnapshot(`
     Object {
       "children": <b
-        className="text-green-700 leading-6 font-bold"
+        className="text-[#1a8917] hover:text-[#0f730c] leading-6 font-bold"
       >
         Create one
       </b>,
