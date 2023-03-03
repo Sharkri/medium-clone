50520af06d01ff892117c98484a3d885aa3d5df2
Sharkri <sharkdude955@gmail.com>
2023-03-02 18:41:16 -0600

    Add max width for post images

--- src/components/BlogPost.tsx
+++ src/components/BlogPost.tsx
@@ -63,7 +63,7 @@ export default function BlogPost() {
             </div>
           </header>
 
-          <div className="prose max-sm:prose-p:text-[18px] max-sm:prose-h1:text-[20px] max-sm:prose-h2:text-[18px] max-w-full prose-img:mx-auto prose-pre:bg-[#282c34] font-content-sans prose-headings:font-sohne-bold">
+          <div className="prose max-sm:prose-p:text-[18px] max-sm:prose-h1:text-[20px] max-sm:prose-h2:text-[18px] max-w-full prose-img:mx-auto prose-img:max-h-[696px] prose-pre:bg-[#282c34] font-content-sans prose-headings:font-sohne-bold">
             <div className="not-prose">
               <h1 className="text-[32px] tracking-[-0.256px] font-sohne-bold">
                 {post.title}
