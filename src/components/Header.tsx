d1713048cc32821fc76a218107d5809663f34f2e
Sharkri <sharkdude955@gmail.com>
2023-03-03 07:30:39 -0600

    Make header disappear on scroll down and appear on scroll up

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,10 +7,17 @@ import Nav from "./Nav";
 import OpenModalButton from "./OpenModalButton";
 import SignUpPage from "./SignUpPage";
 import SignInPage from "./SignInPage";
+import useScrollDirection from "./useScrollDirection";
 
 export default function Header({ user }: { user: User | null }) {
+  const scrollDirection = useScrollDirection();
+
   return (
-    <header className="sticky top-0 z-10 min-h-[57px] bg-white px-6 border-b border-b-subtle-white flex items-center">
+    <header
+      className={`sticky ${
+        scrollDirection === "down" ? "-top-[57px]" : "top-0"
+      }  z-10 min-h-[57px] bg-white px-6 border-b border-b-subtle-white flex items-center transition-all duration-250`}
+    >
       <div className="grow flex">
         <Link to="/">
           <LogoWithoutText />
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,16 @@
 import { ReactNode } from "react";
+import useScrollDirection from "./useScrollDirection";
 
 export default function Sidebar({ children }: { children: ReactNode }) {
+  const scrollDirection = useScrollDirection();
+
   return (
     <>
-      <aside className="h-auto self-start sticky top-[57px] max-w-[368px] grow flex justify-center pl-8 pr-6 max-lg:hidden">
+      <aside
+        className={`h-auto self-start sticky ${
+          scrollDirection === "down" ? "top-0" : "top-[57px]"
+        } max-w-[368px] grow flex justify-center pl-8 pr-6 max-lg:hidden transition-all duration-250`}
+      >
         <div className="absolute left-0 h-screen border-l border-l-subtle-white" />
 
         {children}
