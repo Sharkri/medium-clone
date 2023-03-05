7532f1174d71c34f90916359b9f74f364586bac5
Sharkri <sharkdude955@gmail.com>
2023-03-04 18:34:32 -0600

    Change to onMouseDown on Modal

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -9,7 +9,7 @@ function Modal() {
       className="fixed left-0 right-0 top-0 bottom-0 z-20 flex justify-center items-center bg-white/95 animate-fade-in"
       data-testid="modal"
       id="modal"
-      onClick={(e) => {
+      onMouseDown={(e) => {
         const modalEdgeClicked = (e.target as HTMLElement).id === "modal";
         // Close modal if clicked outside of modal-content (i.e. the edge of modal)
         if (modalEdgeClicked) setModalOpen(false);
