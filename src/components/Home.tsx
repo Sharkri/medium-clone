1ea492d218ffd44373bc86ef012597a92020dd84
Sharkri <sharkdude955@gmail.com>
2023-03-05 00:21:27 -0600

    Add Get Started button

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,6 +4,7 @@ import Hero from "./Hero";
 import HomeFeed from "./HomeFeed";
 import OpenModalButton from "./OpenModalButton";
 import SignInPage from "./SignInPage";
+import SignUpPage from "./SignUpPage";
 
 function Home() {
   return (
@@ -15,10 +16,21 @@ function Home() {
           </Link>
 
           <nav className="flex items-center gap-6 text-sm">
-            <Link to="#">Our story</Link>
-            <Link to="#">Membership</Link>
-            <Link to="#">Write</Link>
-            <OpenModalButton element={<SignInPage />}>Sign In</OpenModalButton>
+            <div className="flex gap-6 max-md:hidden">
+              <Link to="#">Our story</Link>
+              <Link to="#">Membership</Link>
+              <Link to="#">Write</Link>
+            </div>
+            <OpenModalButton element={<SignInPage />} className="max-sm:hidden">
+              Sign In
+            </OpenModalButton>
+
+            <OpenModalButton
+              element={<SignUpPage />}
+              className="bg-lightblack text-white rounded-full px-4 py-2 transition-colors duration-300 hover:bg-black"
+            >
+              Get Started
+            </OpenModalButton>
           </nav>
         </div>
       </header>
