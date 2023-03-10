7e166365a15b05956dcb84e0e6c7dff5cd3a3c7c
Sharkri <sharkdude955@gmail.com>
2023-03-10 01:31:12 -0600

    Remove useless fragment

--- src/components/main/Sidebar.tsx
+++ src/components/main/Sidebar.tsx
@@ -5,16 +5,14 @@ export default function Sidebar({ children }: { children: ReactNode }) {
   const scrollDirection = useScrollDirection();
 
   return (
-    <>
-      <aside
-        className={`h-auto self-start sticky ${
-          scrollDirection === "down" ? "top-0" : "top-[57px]"
-        } max-w-[368px] max-xlg:max-w-[352px] grow flex pl-9 pr-6 max-lg:hidden transition-all duration-250`}
-      >
-        <div className="absolute left-0 h-screen border-l border-l-subtle-white" />
+    <aside
+      className={`h-auto self-start sticky ${
+        scrollDirection === "down" ? "top-0" : "top-[57px]"
+      } max-w-[368px] max-xlg:max-w-[352px] grow flex pl-9 pr-6 max-lg:hidden transition-all duration-250`}
+    >
+      <div className="absolute left-0 h-screen border-l border-l-subtle-white" />
 
-        {children}
-      </aside>
-    </>
+      {children}
+    </aside>
   );
 }
