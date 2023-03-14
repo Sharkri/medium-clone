7cb821e88bd100bfb8784bc02194c23c9b9b624d
Sharkri <sharkdude955@gmail.com>
2023-03-14 03:36:27 -0500

    Fix decimals in reading time not working

--- src/helper-functions/calculateReadingTime.ts
+++ src/helper-functions/calculateReadingTime.ts
@@ -1,12 +1,12 @@
 export default function calculateReadingTime(wordCount: number) {
-  const [minutes, decimals] = (wordCount / 200)
-    .toString()
-    .split(".")
-    .map(Number);
+  // The number before the decimal is minutes and the number after is the seconds
+  const divided = wordCount / 200;
 
+  const decimals = divided % 1;
   const seconds = decimals * 0.6 * 100;
+  const minutes = Math.floor(divided);
 
-  // round up
+  // round up if greater than 30 sec
   if (seconds > 30) return minutes + 1;
 
   return minutes;
