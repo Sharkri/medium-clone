a8a1f6dbae6e892e3287cd2841ad73d36d5c39e4
Sharkri <sharkdude955@gmail.com>
2023-03-14 10:17:11 -0500

    Make component not get unmounted so state is saved

--- src/components/pages/create_post_page/CreatePost.tsx
+++ src/components/pages/create_post_page/CreatePost.tsx
@@ -28,78 +28,83 @@ export default function CreatePost() {
     return () => window.removeEventListener("beforeunload", onBeforeClose);
   }, [blogContents, title]);
 
-  if (isPublishing)
-    return (
-      <PublishPost
-        title={title}
-        description={description}
-        blogContents={blogContents}
-        onGoBack={() => setIsPublishing(false)}
-      />
-    );
-
   return (
-    <div className="flex flex-col items-center mb-12 pt-4">
-      <div className="flex gap-3 text-sm">
-        <button
-          className="flex items-center bg-lightblack px-4 py-2 text-white rounded-full"
-          onClick={() => setIsEditing(!isEditing)}
-        >
-          {isEditing ? (
-            <>
-              <i className="fa-regular fa-eye text-neutral-200 mr-2" />
-              Preview
-            </>
-          ) : (
-            <>
-              <i className="fa-regular fa-edit text-neutral-200 mr-2" />
-              Edit
-            </>
-          )}
-        </button>
-        <button
-          onClick={() => setIsPublishing(true)}
-          className="bg-green text-white rounded-full px-4"
-        >
-          Publish
-        </button>
+    <>
+      <div className={isPublishing ? "block" : "hidden"}>
+        <PublishPost
+          title={title}
+          description={description}
+          blogContents={blogContents}
+          onGoBack={() => setIsPublishing(false)}
+        />
       </div>
 
       <div
-        className={`flex flex-col w-full max-w-[740px] px-5 py-1 ${
-          !isEditing && "hidden"
+        className={`flex flex-col items-center mb-12 pt-4 ${
+          isPublishing ? "hidden" : ""
         }`}
       >
-        <TextareaAutosize
-          placeholder="Title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value.replace(/\n/g, ""))}
-          className="font-title mb-1 text-[42px] leading-[52.5px] outline-none resize-none"
-        />
-        <TextareaAutosize
-          placeholder="Description"
-          value={description}
-          onChange={(e) => setDescription(e.target.value.replace(/\n/g, ""))}
-          className="font-content-serif mb-3 text-[26px] leading-[42.4px] text-grey outline-none resize-none"
-        />
-        <TextareaAutosize
-          className="font-content-serif text-[21px] leading-[33.18px] outline-none resize-none"
-          value={blogContents}
-          onChange={(e) => setBlogContents(e.target.value)}
-          placeholder="Tell your story..."
-          autoFocus
-        />
-      </div>
+        <div className="flex gap-3 text-sm">
+          <button
+            className="flex items-center bg-lightblack px-4 py-2 text-white rounded-full"
+            onClick={() => setIsEditing(!isEditing)}
+          >
+            {isEditing ? (
+              <>
+                <i className="fa-regular fa-eye text-neutral-200 mr-2" />
+                Preview
+              </>
+            ) : (
+              <>
+                <i className="fa-regular fa-edit text-neutral-200 mr-2" />
+                Edit
+              </>
+            )}
+          </button>
+          <button
+            onClick={() => setIsPublishing(true)}
+            className="bg-green text-white rounded-full px-4"
+          >
+            Publish
+          </button>
+        </div>
 
-      {!isEditing && (
-        <div className="max-w-[680px]">
-          <BlogMarkdownWithTitleAndDesc
-            title={title}
-            description={description}
-            blogContents={blogContents}
+        <div
+          className={`flex flex-col w-full max-w-[740px] px-5 pt-2 ${
+            !isEditing && "hidden"
+          }`}
+        >
+          <TextareaAutosize
+            placeholder="Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value.replace(/\n/g, ""))}
+            className="font-title mb-1 text-[42px] leading-[52.5px] outline-none resize-none"
+          />
+          <TextareaAutosize
+            placeholder="Description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value.replace(/\n/g, ""))}
+            className="font-content-serif mb-3 text-[26px] leading-[42.4px] text-grey outline-none resize-none"
+          />
+          <TextareaAutosize
+            className="font-content-serif text-[21px] leading-[33.18px] outline-none resize-none"
+            value={blogContents}
+            onChange={(e) => setBlogContents(e.target.value)}
+            placeholder="Tell your story..."
+            autoFocus
           />
         </div>
-      )}
-    </div>
+
+        {!isEditing && (
+          <div className="max-w-[680px]">
+            <BlogMarkdownWithTitleAndDesc
+              title={title}
+              description={description}
+              blogContents={blogContents}
+            />
+          </div>
+        )}
+      </div>
+    </>
   );
 }
