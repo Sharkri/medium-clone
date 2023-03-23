41d7e78075ead764e889c7be3a1ef2b940315ec6
Sharkri <sharkdude955@gmail.com>
2023-03-22 21:45:55 -0500

    Add no stories found message to profile posts

--- src/components/pages/profile_page/ProfilePosts.tsx
+++ src/components/pages/profile_page/ProfilePosts.tsx
@@ -8,14 +8,16 @@ export default function ProfilePosts({ user }: { user: UserData }) {
   // only posts where author is the user
   const options = [where("authorUid", "==", user.uid)];
 
-  const [posts, setPosts] = useState<Post[]>([]);
+  const [posts, setPosts] = useState<Post[] | null>(null);
 
   return (
     <div className="px-6 flex flex-col gap-6">
       <Posts
         options={options}
         posts={posts}
-        onPostChange={(newPosts: Post[]) => setPosts(posts.concat(newPosts))}
+        onPostChange={(newPosts: Post[]) =>
+          setPosts((posts || []).concat(newPosts))
+        }
       />
     </div>
   );
