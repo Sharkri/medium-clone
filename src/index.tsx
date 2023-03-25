24a351acfa024ca2949965b7abe8eb39d63a3049
Sharkri <sharkdude955@gmail.com>
2023-03-25 18:24:01 -0500

    Fix 404 page appearing when refreshing

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/medium-clone">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
