5077c1208bfa51b5d4cc73a823221d5408300588
Sharkri <sharkdude955@gmail.com>
2023-03-25 20:20:26 -0500

    Fix react router refresh problem

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/medium-clone">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
