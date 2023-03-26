4fb45c459b10fed09a93fcea2b8116600b31197c
Sharkri <sharkdude955@gmail.com>
2023-03-25 21:54:03 -0500

    Add _blank

--- src/components/helper-components/BlogMarkdown.tsx
+++ src/components/helper-components/BlogMarkdown.tsx
@@ -5,6 +5,7 @@ import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 export default function BlogMarkdown({ text }: { text: string }) {
   return (
     <ReactMarkdown
+      linkTarget="_blank"
       components={{
         code({ node, inline, className, children, ...props }) {
           const match = /language-(\w+)/.exec(className || "");
--- src/components/pages/logged_out_page/LoggedOutHeader.tsx
+++ src/components/pages/logged_out_page/LoggedOutHeader.tsx
@@ -25,7 +25,13 @@ export default function LoggedOutHeader({
 
         <nav className="flex items-center gap-6 text-sm">
           <div className="flex gap-6 max-md:hidden">
-            <a href="https://github.com/Sharkri/medium-clone">Github Repo</a>
+            <a
+              href="https://github.com/Sharkri/medium-clone"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github Repo
+            </a>
           </div>
           <OpenModalButton
             element={<SignInOptions />}
